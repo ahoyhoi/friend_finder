fa3c42b5b5530d9cce5bdd3dbdb2370c6ffa391d
Hoi So <miss.hoiso@gmail.com>
2019-10-26 15:04:48 -0400

    upload

---
+++ app/data/friends.js
@@ -0,0 +1,50 @@
+
+
+// Templated Friends Data
+var friendsData = [
+    {
+      name: "Eleven",
+      photo: "https://gl-images.condecdn.net/image/ndymDwvjvm2/crop/405/f/millie-bobby-brown_glamour_30oct17_gettyimages-867236968_p.jpg",
+      scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    },
+    {
+      name: "Mike",
+      photo:"https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2018/01/calupurnia_170924_0853v2_print_1_copy_-_h_2018.jpg",
+      scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+      name: "Dustin",
+      photo:"https://cdn1.clickthecity.com/profiles/400/20281_1.jpg",
+      scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+      name: "Will",
+      photo:"https://pmcvariety.files.wordpress.com/2019/01/noah-schnapp-e1548309620989.jpg?w=1000&h=563&crop=1",
+      scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+      name: "Nancy",
+      photo:"https://upload.wikimedia.org/wikipedia/commons/7/7d/Natalia_Dyer_by_Gage_Skidmore.jpg",
+      scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+    },
+    {
+      name: "Lucas",
+      photo:"https://www.famousbirthdays.com/headshots/caleb-mclaughlin-7.jpg",
+      scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+    },
+    {
+      name: "Max",
+      photo:"https://upload.wikimedia.org/wikipedia/commons/3/33/Sadie_Sink_by_Gage_Skidmore.jpg",
+      scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+    },
+    {
+      name: "Jonathan",
+      photo:"https://i2.wp.com/metro.co.uk/wp-content/uploads/2017/05/pri_39772222-e1494851250605.jpg?quality=90&strip=all&zoom=1&resize=644%2C520&ssl=1",
+      scores: [5, 4, 5, 4, 2, 3, 3, 3, 2, 1]
+    }
+  ];
+  
+  
+  
+  // Export to API Route
+  module.exports = friendsData;
